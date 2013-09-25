coherence-project/Coherence

commit c0902639caed7abb8b74b6f26e543483396f9ba3

Mark stuff not used elsewhere as private ('-' prefix).

coherence/upnp/core/utils.py
@@ -910,18 +910,18 @@ class _tz(tzinfo):
     def dst(self,dt):
         return timedelta(0)
 
-class CET(_tz):
+class _CET(_tz):
     _offset = timedelta(minutes=60)
     _name = 'CET'
 
-class CEST(_tz):
+class _CEST(_tz):
     _offset = timedelta(minutes=120)
     _name = 'CEST'
 
-bdates = [ datetime(1997,2,28,17,20,tzinfo=CET()),   # Sebastian Oliver
-           datetime(1999,9,19,4,12,tzinfo=CEST()),   # Patrick Niklas
-           datetime(2000,9,23,4,8,tzinfo=CEST()),    # Saskia Alexa
-           datetime(2003,7,23,1,18,tzinfo=CEST()),   # Mara Sophie
+_bdates = [datetime(1997,2,28,17,20,tzinfo=CET()),  # Sebastian Oliver
+           datetime(1999,9,19,4,12,tzinfo=CEST()),  # Patrick Niklas
+           datetime(2000,9,23,4,8,tzinfo=CEST()),   # Saskia Alexa
+           datetime(2003,7,23,1,18,tzinfo=CEST()),  # Mara Sophie
                                                      # you are the best!
          ]
 

Toysoft/DMM-python-coherence

commit 47d004f15f1c6951fafe78d36be5ec99486810ff

fix smal error introduced in c0902639caed7abb8b74b6f26e543483396f9ba3

coherence/upnp/core/utils.py
@@ -783,4 +783,4 @@ _bdates = [datetime(1997,2,28,17,20,tzinfo=_CET()),  # Sebastian Oliver
          ]
 
 def datefaker():
-    return random.choice(bdates)
+    return random.choice(_bdates)